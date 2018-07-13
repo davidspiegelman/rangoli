51388703531c0923846ebd52bba39114024db41b
davidspiegelman <davids@eero.com>
2018-07-13 12:03:38 -0700

    Create rangoli.py

---
+++ rangoli.py
@@ -0,0 +1,7 @@
+def print_rangoli(size, d="-", p="abcdefghijklmnopqrstuvwxyz"):    
+    p = (p * ((size // len(p)) + 1))[::-1][-size:]
+    f = lambda y: list(range(0, y)) + list(range(y - 2, -1, -1))
+    for i in f(size):
+        b = [d] * (size - 1 - i)
+        print(d.join(b + list(map(lambda x: p[x], f(i + 1))) + b))
+        

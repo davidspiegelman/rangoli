3cf46d90f7181e2e75a48255b86e5d5c5e1fa4e3
David Spiegelman <david94609@gmail.com>
2020-07-28 19:06:52 -0700

    Changed list to generator expression

--- rangoli.py
+++ rangoli.py
@@ -1,6 +1,6 @@
 def print_rangoli(size, d="-", p="abcdefghijklmnopqrstuvwxyz"):    
     p = (p * (((size - 1) // len(p)) + 1))[::-1][-size:]
-    f = lambda y: list(range(y)) + list(range(y - 2, -1, -1))
+    f = lambda y: (y - 1 - abs(x) for x in range(y - 1, -y, -1))
     for i in f(size):
         b = [d] * (size - 1 - i)
         print(d.join(b + [ p[x] for x in f(i + 1) ] + b))

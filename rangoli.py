f6cbbda3d8a695ac275500fff6cad1f8fc988f6b
David Spiegelman <david94609@gmail.com>
2020-07-30 20:06:49 -0700

    Requires Python 3.8

--- rangoli.py
+++ rangoli.py
@@ -1,7 +1,5 @@
 def print_rangoli(size, d="-", p="abcdefghijklmnopqrstuvwxyz"):    
     p = (p * (((size - 1) // len(p)) + 1))[::-1][-size:]
-    f = lambda y: (y - 1 - abs(x) for x in range(y - 1, -y, -1))
-    for i in f(size):
-        b = [d] * (size - 1 - i)
-        print(d.join(b + [ p[x] for x in f(i + 1) ] + b))
-        
+    f = lambda y: (y - abs(x) for x in range(y , -y - 1, -1))
+    for i in f(s:= size - 1):
+        print(d.join((b := [d] * (s - i)) + [p[x] for x in f(i)] + b))
